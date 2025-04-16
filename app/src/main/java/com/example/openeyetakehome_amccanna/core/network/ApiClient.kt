47aad4847e308cfa42aac89a83345787d5b1be2a
Alex <a.mccanna@hotmail.com>
2025-04-16 12:20:59 -0700

    Initial commit

---
+++ app/src/main/java/com/example/openeyetakehome_amccanna/core/network/ApiClient.kt
@@ -0,0 +1,4 @@
+package com.example.openeyetakehome_amccanna.core.network
+
+class ApiClient {
+}
---
+++ app/src/main/java/com/example/openeyetakehome_amccanna/feature/main/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.openeyetakehome_amccanna.feature.main
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.openeyetakehome_amccanna.core.ui.theme.OpeneyeTakeHomeAMcCannaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OpeneyeTakeHomeAMcCannaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Alex",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OpeneyeTakeHomeAMcCannaTheme {
+        Greeting("Android")
+    }
+}
